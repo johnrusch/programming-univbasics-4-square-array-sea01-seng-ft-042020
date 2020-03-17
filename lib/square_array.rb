07298d1351c171b2118bfaed771680bcb6f6eb50
John Rusch <johnrusch7@gmail.com>
2020-03-17 23:35:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,11 +2,16 @@ def square_array(array)
   
   counter = 0 
   
-  new_array
+  new_array = []
   
   while counter < array.length do
     
     new_array.push(array[counter]**)
     
     counter += 1 
+    
+  end
+  
+  new_array
+  
 end
